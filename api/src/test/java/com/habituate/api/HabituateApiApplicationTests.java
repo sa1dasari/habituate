af8df1b07f6520d477c152e452618803e4d2d8b3
sa1dasari <sawandasari2@gmail.com>
2026-09-11 21:07:56 -0400

    feat(api): add Spring Boot health service

---
+++ api/src/test/java/com/habituate/api/HabituateApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.habituate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabituateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
